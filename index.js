404d20278adfaafcb910b1469351c40f5413ba99
grvsharma1810 <gs8280601@gmail.com>
2021-06-07 20:11:25 +0530

    fix: build error

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const dotenv = require('dotenv')
 const morgan = require('morgan');
 const initializeDBConnection = require('./db/db.connect');
-const User = require('./models/User');
-const Post = require('./models/Post');
+// const User = require('./models/User');
+// const Post = require('./models/Post');
 
 dotenv.config();
-app.use(cors())
+const app = express();
 app.use(morgan('dev'))
 app.use(express.json());
+app.use(cors())
 
-// initializeDBConnection();
+initializeDBConnection();
 
-app.get('/', async (req, res) => {
-    try {
-
+app.get('/', (req, res) => {
         // const posts = await Post.find();
         // console.log(posts);
 
@@ -32,12 +30,9 @@ app.get('/', async (req, res) => {
         // })
         // res.send(user);
         res.send("Oh Boy")
-    } catch (error) {
-        res.send("ERROR")
-    }
 })
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, err => {
+app.listen(PORT, () => {
     console.log("Server started at port " + PORT);
 })
